c941b2badb79ddb54c0068975489366f0d241f49
Shiven Singhal <shiven.singhal77@gmail.com>
2022-06-07 20:52:55 +0530

    port changes

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.get('/',(req,res)=>{
 app.get('/weather',(req,res)=>{
     res.render('weather');
 })
-
-app.listen(3000, () => {
+const port= process.env.PORT||3000
+app.listen(port, () => {
     console.log('Serving on port 3000')
 })
